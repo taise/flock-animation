d11f63565cc5fa5ede35499e29cc0492705b5127
taise <taise515@gmail.com>
2016-04-10 09:45:09 +0900

    gulpタスクの追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
-var gulp = require('gulp');
+var gulp    = require('gulp'),
+    uglify  = require("gulp-uglify"),
+    concat  = require("gulp-concat"),
+    plumber = require("gulp-plumber");
+
 var bs = require('browser-sync').create();
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('default', ['js.concat.vendor', 'browser-sync'], function() {
+    console.log('It works!');
 });
 
 gulp.task('browser-sync', function() {
@@ -13,4 +17,29 @@ gulp.task('browser-sync', function() {
           index: "space.html"
         }
     });
+});
+
+gulp.task('js.concat.vendor', function() {
+    return gulp.src([
+        'node_modules/d3/d3.js',
+        'node_modules/d3-random/build/d3-random.js'
+      ])
+      .pipe(plumber({
+            errorHandler: (error) => {
+              notify('js.concat.vendor', error);
+            }
+      }))
+      .pipe(concat('vendor.js'))
+      .pipe(gulp.dest('./js'));
+});
+
+gulp.task('js.uglify.vendor', function() {
+  return gulp.src('js/vendor.js')
+      .pipe(plumber({
+            errorHandler: (error) => {
+              notify('js.uglify.vendor', error);
+            }
+      }))
+      .pipe(uglify('vendor.min.js'))
+      .pipe(gulp.dest('./js'));
 });
--- js/flock.js
+++ js/flock.js
@@ -1,1 +1,37 @@
-d3.select("body").style("background-color", "black");
+var sideSize   = 600;
+var circleSize = 5;
+
+var space  = d3.select("#space").append("svg");
+var color  = "#ffea00";
+var random = d3_random.randomNormal(sideSize/2, sideSize/8)
+
+space.style("height", sideSize)
+     .style("width",  sideSize)
+     .style("background-color", "#212121");
+
+
+function createCircle(space, elements) {
+  space.selectAll('circle')
+  .data(elements)
+  .enter()
+  .append('circle')
+  .attr({
+      'name': function(d) { return d[0]; },
+      'cx':   function(d) { return d[1]; },
+      'cy':   function(d) { return d[2]; },
+      'r':    function(d) { return d[3]; },
+      'fill': function(d) { return d[4]; }
+  });
+}
+
+
+var elements = [];
+d3.range(50).forEach(function(d) {
+    elements.push([d, random(), random(), circleSize, color]);
+});
+
+createCircle(space, elements);
+
+var circles = space.selectAll("circle")
+                   .data(elements)
+
