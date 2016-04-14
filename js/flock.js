7e1d5276c24f1307eb501b2d14d98a14b5a7d93b
yusaku.omasa <yusaku.omasa@livesense.co.jp>
2016-04-14 13:12:32 +0900

    高速に動く何か

--- js/flock.js
+++ js/flock.js
@@ -1,5 +1,6 @@
 var sideSize   = 600;
 var circleSize = 5;
+var restrict = sideSize / 20
 
 var space  = d3.select("#space").append("svg");
 var color  = "#ffea00";
@@ -26,7 +27,7 @@ function createCircle(space, elements) {
 }
 
 var elements = [];
-d3.range(200).forEach(function(d) {
+d3.range(300).forEach(function(d) {
     elements.push([d, random(), random(), Math.log(random()), color]);
 });
 
@@ -34,27 +35,36 @@ d3.range(200).forEach(function(d) {
 
 var circles = createCircle(space, elements);
 
-var moveTo = function(sideSize) {
-  // sideSize / 10 = (|rx| + |ry| + |rz|) / denomi
+var moveTo = function(restrict) {
+  // restrict = (|rx| + |ry| + |rz|) / denomi
   var rx = Math.random(Math.random()) * 2 - 1;
   var ry = Math.random(Math.random()) * 2 - 1;
   var rz = Math.random(Math.random()) * 2 - 1;
-  var denomi = 10 * (Math.abs(rx) + Math.abs(ry) + Math.abs(rz)) / sideSize
-  return [rx, ry, rz].map(function(d) { return d / denomi})
+  var denomi = (Math.abs(rx) + Math.abs(ry) + Math.abs(rz)) / restrict
+  return [rx, ry, rz].map(function(d) { return d / denomi; })
 }
 
-function swim(sideSize) {
-  var positions = moveTo(sideSize);
+var color = function(d, positions, restrict) {
+  var r = Math.round((d[1] + positions[0]) * restrict * 5 / 255);
+  var g = Math.round((d[2] + positions[1]) * restrict * 5 / 255);
+  var b = 0;
+  var a = Math.abs(positions[2] / restrict);
+  return ["rgba(" + r, g, b, a + ")"].join(', ');
+}
+
+function swim(restrict) {
+  var positions = moveTo(restrict);
   // positionとcolorをひも付け
 
   d3.selectAll(".fish").transition()
          .delay(50)
-         .duration(function(d) { return d[1]*5; })
+         .duration(function(d) { return 1; })
          .ease("cubic-out")
          .attr("cx", function(d) { return d[1] + positions[0]; })
          .attr("cy", function(d) { return d[2] + positions[1]; })
-         .attr("r",  function(d) { return d[3] + positions[2] * 0.05; });
-  console.log(plusX, plusY)
+         .attr("r",  function(d) { return d[3] + positions[2] * 0.05; })
+         .attr("fill", function(d) { return color(d, positions, restrict); });
+  console.log(positions);
 }
 
-setInterval(function() { swim(sideSize); }, 1000);
+setInterval(function() { swim(restrict); }, 100);
